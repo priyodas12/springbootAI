4d1533a70c16ea1dbc81ba0abd3fb32c9b5a87ca
Priyobrato.Das <priyodas12@gmail.com>
2024-10-30 23:45:32 +0530

    Initial commit

---
+++ src/main/java/tech/springboot/springbootai/SpringbootAiApplication.java
@@ -0,0 +1,13 @@
+package tech.springboot.springbootai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/springboot/springbootai/SpringbootAiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.springboot.springbootai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
